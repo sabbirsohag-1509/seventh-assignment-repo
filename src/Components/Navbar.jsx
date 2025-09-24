f182b59b004b937d4cc059384663acc03a554b82
sabbirsohag-1509 <sabbirhossainsohag1509@gmail.com>
2025-09-24 22:53:34 +0600

    nav bar done

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+    <div>
+        <div className="navbar bg-base-100 shadow-sm px-4 lg:px-20">
+        <div className="flex-1"> 
+        <a className="btn btn-ghost text-xl">CS — Ticket System</a>
+        </div>
+        <div className="flex-none hidden lg:flex ">
+            <ul className='menu menu-horizontal p-0 pr-5'>
+                <li><a className='text-sm font-semibold text-gray-600'>Home</a></li>
+                <li><a className='text-sm font-semibold text-gray-600'>FAQ</a></li>
+                <li><a className='text-sm font-semibold text-gray-600'>Changelog</a></li>
+                <li><a className='text-sm font-semibold text-gray-600'>Blog</a></li>
+                <li><a className='text-sm font-semibold text-gray-600'>Download</a></li>
+                <li><a className='text-sm font-semibold text-gray-600'>Contact</a></li>
+            </ul>
+        </div>
+        <div className="flex-none">
+            <a className="btn bg-gradient-to-r from-[#632EE3] to-[#9F62F2] text-white rounded-md">+ New Ticket</a>
+        </div>
+        </div>
+    </div>
+    );
+};
+
+export default Navbar;
