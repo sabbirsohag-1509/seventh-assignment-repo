782f86d592da3138e36212f92505954b117c25d9
sabbirsohag-1509 <sabbirhossainsohag1509@gmail.com>
2025-09-25 18:59:39 +0600

    after designing & input all data in card

--- src/Components/Ticket-Cards/TicketCard.jsx
+++ src/Components/Ticket-Cards/TicketCard.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { CiCalendar } from "react-icons/ci";
+import { GoDotFill } from "react-icons/go";
 
 const TicketCard = ({card}) => {
 
@@ -6,23 +8,27 @@ const TicketCard = ({card}) => {
 
     return (
         <div> 
-            <div className='w-[380px] h-auto p-5 shadow-[0_0_15px_rgba(0,0,0,0.3)] rounded-md'> 
+            <div className='w-[390px] h-[170px] p-5 shadow-[0_0_15px_rgba(0,0,0,0.3)] rounded-md'> 
                 <div className='flex justify-between'> 
-                    <h1 className='text-lg'>Login Issues - Can't Access Account</h1>
-                    <p className='text-lg '>Open</p>
+                    <h1 className='text-lg'>{card.title}</h1>
+                    <p className='flex items-center text-lg '>
+                        <GoDotFill className="text-2xl" />{card.status}
+                    </p>
                 </div>
                 <div> 
-                    <p className='text-sm text-gray-500 pt-2'>Customer is unable to log in to their account. They've tried resetting their password multiple times but still...</p>
+                    <p className='text-sm text-gray-500 pt-2'>{card.description}</p>
                 </div>
 
                 <div className='flex gap-5 pt-3'> 
                     <div className='flex gap-3 pt-2'> 
-                        <p className='text-sm'>#1001</p>
-                        <p className='text-sm'>HIGH PRIORITY</p>
+                        <p className='text-sm text-gray-700'>#{card.id}</p>
+                        <p className='text-sm'>{card.priority}</p>
                     </div>
                     <div className='flex gap-3 pt-2'> 
-                        <p className='text-sm'>Michael Brown</p>
-                        <p className='text-sm'>1/17/2024</p>
+                        <p className='text-sm'>{card.reporter}</p>
+                        <p className='flex items-center gap-1 text-sm'> 
+                            <CiCalendar className='' />{card.createdAt}
+                        </p>
                     </div>
                 </div>   
             </div>
--- src/Components/Ticket-Cards/TicketCards.jsx
+++ src/Components/Ticket-Cards/TicketCards.jsx
@@ -10,13 +10,16 @@ const TicketCards = ({fetchPromise}) => {
        <div className='container mx-auto flex gap-5'> 
          <div className='w-[1000px]'>
             <h1 className='mt-10 text-[27px] font-semibold'>Customer Tickets</h1>
-            <div className=' grid grid-cols-2 gap-8 mt-8'> 
+
+            <div className=' grid grid-cols-2 gap-5 mt-8 '> 
                 {
                 cards.map(card => <TicketCard key={card.id} card={card}></TicketCard>)
                 }
             </div>
             </div>
-            <div className="w-[300px] bg-gray-200 p-4 rounded-lg mt-10">
+
+
+            <div className="w-[250px] bg-gray-200 p-4 rounded-lg mt-10 mr-2">
                 <h2 className="text-[27px] font-semibold">Task Status</h2>
             </div>
        </div>
