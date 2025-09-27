d1a196faa477a9f9d8325ac4586df424542677ce
sabbirsohag-1509 <sabbirhossainsohag1509@gmail.com>
2025-09-27 12:41:05 +0600

    after all final recap before submit

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -5,24 +5,24 @@ const Banner = ({progressCount, resolvedCount}) => {
     return (
         <div className='max-w-[1150px] mx-auto mt-5 lg:mt-14 flex flex-col lg:flex-row gap-3 lg:gap-10 h-auto lg:h-48 '>
             
-            <div className='flex flex-col justify-center items-center text-center  bg-gradient-to-r from-[#632EE3] to-[#9F62F2] w-full lg:w-[50%] rounded-2xl text-white relative '> 
-                <div className=''> 
+            <div className='flex flex-col justify-center items-center text-center  bg-gradient-to-r from-[#632EE3] to-[#9F62F2] w-full lg:w-[50%] rounded-2xl text-white relative p-4'> 
+                <div> 
                     
                     <h1 className='text-2xl font-bold'>In-Progress</h1>
-                    <p className='text-4xl font-semibold pt-3'>{progressCount}</p>
+                    <p className='text-3xl lg:text-4xl font-semibold pt-1 lg:pt-3'>{progressCount}</p>
                 </div>
-                    <div className="w-64 absolute inset-0 bg-[url('/vector1.png')] bg-cover bg-center "></div>
-                    <div className="w-64 absolute inset-0 bg-[url('/vector1.png')] bg-cover bg-center scale-x-[-1] left-77 "></div>
+                    <div className="w-47 lg:w-64 absolute left-0 inset-0 bg-[url('/vector1.png')] bg-cover bg-center "></div>
+                    <div className="w-47 lg:w-64 absolute right-0 inset-0 bg-[url('/vector1.png')] bg-cover bg-center scale-x-[-1] left-56 lg:left-77 "></div>
             </div> 
 
-            <div className='flex flex-col justify-center items-center text-center bg-gradient-to-r from-[#54CF68] to-[#00827A] w-full lg:w-[50%] rounded-2xl text-white relative'> 
+            <div className='flex flex-col justify-center items-center text-center bg-gradient-to-r from-[#54CF68] to-[#00827A] w-full lg:w-[50%] rounded-2xl text-white relative p-4'> 
                 <div> 
                     <h1 className='text-2xl font-bold'>Resolved</h1>
-                    <p className='text-4xl font-semibold pt-3'>{resolvedCount}</p>
+                    <p className='text-3xl lg:text-4xl font-semibold pt-1 lg:pt-3'>{resolvedCount}</p>
                 </div>
 
-                <div className="w-64 absolute inset-0 bg-[url('/vector1.png')] bg-cover bg-center "></div>
-                <div className="w-64 absolute inset-0 bg-[url('/vector1.png')] bg-cover bg-center scale-x-[-1] left-77 "></div>
+                <div className="w-47 lg:w-64 absolute right-0 inset-0 bg-[url('/vector1.png')] bg-cover bg-center "></div>
+                <div className="w-47 lg:w-64 absolute inset-0 bg-[url('/vector1.png')] bg-cover bg-center scale-x-[-1] left-56 lg:left-77 "></div>
             </div>
         </div>
     );
